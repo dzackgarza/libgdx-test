7f3850d9cf0e683e8479d0ee21664f34357b1eee
D. Zack Garza <Dzackgarza@gmail.com>
2016-08-01 21:45:16 -0700

    First commit.

---
+++ desktop/src/com/myrpg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.myrpg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.myrpg.game.MyRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyRPG(), config);
+	}
+}
---
+++ html/src/com/myrpg/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.myrpg.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.myrpg.game.MyRPG;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyRPG();
+        }
+}
